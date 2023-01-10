25e3d14bf5904818cd0e75b4c28c4bf41565c439
CyberJoel1 <joel_sec111997@hotmail.com>
2023-01-10 00:14:32 -0500

    host vercel

--- utils/Config/host.ts
+++ utils/Config/host.ts
@@ -1,4 +1,5 @@
 
 export const CONFIG = {
-    host:'https://waroombackend-production.up.railway.app'
+    host:'https://waroombackend-production.up.railway.app',
+    hostSelf:process.env.VERCEL_URL
 }
